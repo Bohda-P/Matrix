37eaa6a75dbe1accff8874b80530f21f4e7ae519
Bohdan <bogdanpaseka2000@gmail.com>
2020-08-29 16:58:11 +0300

    initial

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Matrix from '../Matrix/Matrix'
+import Form from '../Form/Form'
+
+function App (){
+    return (
+        <>
+           <Matrix/>
+           <Form/>
+        </>
+        
+    )
+ }
+
+export default App
+
